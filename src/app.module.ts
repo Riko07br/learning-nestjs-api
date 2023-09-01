481f2a57a2b22ec66b87297c8cb7fb36d16b3bb2
Natanael Felix <f.natanael.felix.b@gmail.com>
2023-09-01 20:13:50 -0300

    build: add dependencies to main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,19 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthService } from "./auth/auth.service";
 import { AuthController } from "./auth/auth.controller";
+import { UserService } from "./user/user.service";
+import { UserController } from "./user/user.controller";
+import { PrismaModule } from "./prisma/prisma.module";
+import { AuthModule } from "./auth/auth.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-    imports: [],
-    controllers: [AppController, AuthController],
-    providers: [AppService, AuthService],
+    imports: [
+        PrismaModule,
+        AuthModule,
+        ConfigModule.forRoot({ isGlobal: true }),
+    ],
+    controllers: [AppController, AuthController, UserController],
+    providers: [AppService, AuthService, UserService],
 })
 export class AppModule {}
