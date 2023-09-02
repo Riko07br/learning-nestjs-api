83a74b23710b6861ff0228a23d7038e2eec20228
Natanael Felix <f.natanael.felix.b@gmail.com>
2023-09-01 22:06:01 -0300

    feat(auth): add jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,14 @@ import { UserController } from "./user/user.controller";
 import { PrismaModule } from "./prisma/prisma.module";
 import { AuthModule } from "./auth/auth.module";
 import { ConfigModule } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
     imports: [
         PrismaModule,
         AuthModule,
         ConfigModule.forRoot({ isGlobal: true }),
+        JwtModule,
     ],
     controllers: [AppController, AuthController, UserController],
     providers: [AppService, AuthService, UserService],
